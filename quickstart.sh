0674df865749a493e98fe478960640aa697fcae8
Clinton Skakun <clinton.d.skakun@dedupe.ly>
2026-02-18 17:52:32 -0600

    Added quickstart script plan

---
+++ quickstart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 1. Install sqlite3, docker-ce, go compiler
+# 2. Create boring-ci user and group and add it to docker
+# 3. Compile boring ci and add it to bin
+# 4. Add /etc/boring-ci/config.json
+# 4. Add systemd service file
